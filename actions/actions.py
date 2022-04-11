bbc561b039a88dea760d496a84506b1e58d2ce3f
Zohreh Fallahnejad <z.fallahnejad@gmail.com>
2022-04-11 16:33:45 +0430

    Add files via upload

---
+++ actions/actions.py
@@ -0,0 +1,122 @@
+import logging
+from typing import Dict, Text, Any, List, Optional
+
+from rasa_sdk.events import SlotSet
+from rasa_sdk import Tracker, Action
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+
+logger = logging.getLogger(__name__)
+
+
+class ValidateRestaurantForm(FormValidationAction):
+    """Example of a form validation action."""
+
+    def name(self) -> Text:
+        return "validate_restaurant_form"
+
+    async def required_slots(
+            self,
+            slots_mapped_in_domain: List[Text],
+            dispatcher: "CollectingDispatcher",
+            tracker: "Tracker",
+            domain: "DomainDict",
+    ) -> Optional[List[Text]]:
+        return ["cuisine", "num_people", "outdoor_seating", "feedback"]
+
+    @staticmethod
+    def cuisine_db() -> List[Text]:
+        """Database of supported cuisines."""
+
+        return [
+            "caribbean",
+            "chinese",
+            "french",
+            "greek",
+            "indian",
+            "italian",
+            "mexican",
+        ]
+
+    @staticmethod
+    def is_int(string: Text) -> bool:
+        """Check if a string is an integer."""
+
+        try:
+            int(string)
+            return True
+        except ValueError:
+            return False
+
+    def validate_cuisine(
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate cuisine value."""
+
+        if value.lower() in self.cuisine_db():
+            # validation succeeded, set the value of the "cuisine" slot to value
+            if value.lower() == "indian":
+                return {"cuisine": value, "num_people": 1}
+            else:
+                return {"cuisine": value}
+        else:
+            dispatcher.utter_message(response="utter_wrong_cuisine")
+            # validation failed, set this slot to None, meaning the
+            # user will be asked for the slot again
+            return {"cuisine": None}
+
+    def validate_num_people(
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate num_people value."""
+
+        if self.is_int(value) and int(value) > 0:
+            return {"num_people": value}
+        else:
+            dispatcher.utter_message(response="utter_wrong_num_people")
+            # validation failed, set slot to None
+            return {"num_people": None}
+
+    def validate_outdoor_seating(
+            self,
+            value: Text,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> Dict[Text, Any]:
+        """Validate outdoor_seating value."""
+
+        if isinstance(value, str):
+            if "out" in value:
+                # convert "out..." to True
+                return {"outdoor_seating": True}
+            elif "in" in value:
+                # convert "in..." to False
+                return {"outdoor_seating": False}
+            else:
+                dispatcher.utter_message(response="utter_wrong_outdoor_seating")
+                # validation failed, set slot to None
+                return {"outdoor_seating": None}
+
+        else:
+            # affirm/deny was picked up as True/False by the from_intent mapping
+            return {"outdoor_seating": value}
+
+
+class ResetRestaurantForm(Action):
+    def name(self) -> Text:
+        return "action_reset_restaurant_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        return [SlotSet('num_people', None), SlotSet('outdoor_seating', None), SlotSet('preferences', None),
+                SlotSet('feedback', None)]
